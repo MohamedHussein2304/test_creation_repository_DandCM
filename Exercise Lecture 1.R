3400f879cac3a6778953c2a00cb20b779419c622
Mohamed Hussein <mohamedhussein.mailpro@gmail.com>
2025-10-06 22:40:48 +0200

    lecture1, exo 1

---
+++ Exercise Lecture 1.R
@@ -0,0 +1,13 @@
+
+#Exercise Lecture 1
+
+##Exercise 1 R script
+
+### Create a numeric vector of length 5
+my_vector <- c(4, 8, 15, 16, 23)
+
+### Compute its mean
+vector_mean <- mean(my_vector)
+
+### Print the result with a short message
+print(paste("The mean of the vector is:", vector_mean))
